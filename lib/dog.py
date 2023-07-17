99cbbf61c5f8aca809353afd50f583e4a67b8ef1
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-17 12:57:18 -0700

    resolves

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def display_info(self):
+        print(f"name is {self.name}, breed is {self.breed}")
+
+
+fido = Dog("Fido", "lebrador")
+fido.display_info()
